e0bee7e89402c0955bb8638e7a529d34801991d7
daksha14 <daksha.logicalstreet@gmail.com>
2020-04-04 17:57:19 +0530

    email config in production

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -109,4 +109,15 @@ Rails.application.configure do
   # config.active_record.database_selector = { delay: 2.seconds }
   # config.active_record.database_resolver = ActiveRecord::Middleware::DatabaseSelector::Resolver
   # config.active_record.database_resolver_context = ActiveRecord::Middleware::DatabaseSelector::Resolver::Session
+
+  config.action_mailer.delivery_method = :smtp
+    config.action_mailer.smtp_settings = {
+      address: "smtp.gmail.com",
+      port: 587,
+      domain: "localhost:3000",
+      authentication: "plain",
+      user_name: "daksha.logicalstreet@gmail.com",
+      password: "tnyppftxnlgfgffi",
+      enable_starttls_auto: true
+  }
 end
